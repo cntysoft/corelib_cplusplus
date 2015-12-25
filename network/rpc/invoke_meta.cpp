ae245c24014bc4869468c78d53b6bebbb0d3a12b
SOFTBOY <softboy@fhzc.com>
2015-12-25 16:24:54 +0800

    完成调用元信息

---
+++ network/rpc/invoke_meta.cpp
@@ -0,0 +1,107 @@
+#include "invoke_meta.h"
+
+namespace sn{
+namespace network{
+
+ApiInvokeRequest::ApiInvokeRequest(const QString &name, const QString &method, const QList<QVariant> &args)
+   : m_name(name),
+     m_method(method),
+     m_args(args)
+{}
+
+ApiInvokeRequest& ApiInvokeRequest::setName(const QString &name)
+{
+   m_name = name;
+   return *this;
+}
+
+ApiInvokeRequest& ApiInvokeRequest::setMethod(const QString &method)
+{
+   m_method = method;
+   return *this;
+}
+
+ApiInvokeRequest& ApiInvokeRequest::setArgs(const QList<QVariant> &args)
+{
+   m_args = args;
+   return *this;
+}
+
+ApiInvokeRequest& ApiInvokeRequest::setExtraData(const QByteArray &extraData)
+{
+   m_extraData = extraData;
+   return *this;
+}
+
+QString& ApiInvokeRequest::getName()
+{
+   return m_name;
+}
+
+QString& ApiInvokeRequest::getMethod()
+{
+   return m_method;
+}
+
+QList<QVariant>& ApiInvokeRequest::getArgs()
+{
+   return m_args;
+}
+
+QByteArray& ApiInvokeRequest::getExtraData()
+{
+   return m_extraData;
+}
+
+ApiInvokeResponse::ApiInvokeResponse(const QString &signature, bool status)
+   : m_apiSignature(signature),
+     m_status(status)
+{}
+
+ApiInvokeResponse::setSignature(const QString &signature)
+{
+   m_signature = signature;
+   return *this;
+}
+
+ApiInvokeResponse::setStatus(bool status)
+{
+   m_status = status;
+   return *this;
+}
+
+ApiInvokeResponse::setData(const QMap<QString, QString> &data)
+{
+   m_data = data;
+   return *this;
+}
+
+ApiInvokeResponse::setExtraData(const QByteArray &extraData)
+{
+   m_extraData = extraData;
+   return *this;
+}
+
+QString& ApiInvokeResponse::getSignature()
+{
+   return m_signature;
+}
+
+bool ApiInvokeResponse::getStatus()
+{
+   return m_status;
+}
+
+QMap<QString, QString>& ApiInvokeResponse::getData()
+{
+   return m_data;
+}
+
+QByteArray& ApiInvokeResponse::getExtraData()
+{
+   return m_extraData;
+}
+
+
+}//network
+}//sn
--- network/rpc/invoke_meta.h
+++ network/rpc/invoke_meta.h
@@ -2,20 +2,57 @@
 #define SN_CORELIB_NETWORK_RPC_INVOKE_META_H
 
 #include <QString>
-#include <QStringList>
 #include <QByteArray>
+#include <QList>
+#include <QVariant>
+#include <QPair>
 
 #include "global/global.h"
 
 namespace sn{
 namespace network{
 
-class SN_CORELIB_EXPORT InvokeMeta
+class SN_CORELIB_EXPORT ApiInvokeRequest
 {
+public:
+   ApiInvokeRequest(const QString& name, const QString& method, const QList<QVariant>& args);
+   ApiInvokeRequest& setName(const QString& name);
+   ApiInvokeRequest& setMethod(const QString& method);
+   ApiInvokeRequest& setArgs(const QList<QVariant>& args);
+   ApiInvokeRequest& setExtraData(const QByteArray& extraData);
+   
+   QString& getName();
+   QString& getMethod();
+   QList<QVariant>& getArgs();
+   QByteArray& getExtraData();
 protected:
    QString m_name;
    QString m_method;
-   QStringList m_args;
+   QList<QVariant> m_args;
+   QByteArray m_extraData;
+};
+
+class SN_CORELIB_EXPORT ApiInvokeResponse
+{
+   ApiInvokeResponse(const QString &signature, bool status);
+   
+   ApiInvokeResponse& setSignature(const QString &signature);
+   ApiInvokeResponse& setStatus(bool status);
+   ApiInvokeResponse& setData(const QMap<QString, QString> &data);
+   ApiInvokeResponse& setExtraData(const QByteArray &extraData);
+   ApiInvokeResponse& setError(const QPair<int, QString>& error);
+   
+   QString getSignature();
+   bool getStatus();
+   QMap<QString, QString>& getData();
+   QByteArray& getExtraData();
+   QPair<int, QString>& getError();
+   
+protected:
+   QString m_signature;
+   bool m_status;
+   QMap<QString, QString> m_data;
+   QPair<int, QString> m_error;
    QByteArray m_extraData;
 };
 
